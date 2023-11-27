ea5db02ebf4a6407ae5ea9c7dbb59ff11c1573ae
Mateusz Kopyt <mkopyt@wne.uw.edu.pl>
2023-11-27 01:20:38 +0100

    SpatBenfordPattern() added

--- R/A_spatialWarsaw-package.R
+++ R/A_spatialWarsaw-package.R
@@ -14,12 +14,13 @@
 #' @importFrom dbscan dbscan kNN
 #' @importFrom DescTools PseudoR2
 #' @importFrom dplyr summarise group_by
-#' @importFrom stats binomial glm dist
+#' @importFrom stats binomial glm dist rnorm
 #' @importFrom sp coordinates
-#' @importFrom sf st_coordinates
+#' @importFrom sf st_as_sf st_bbox st_crs st_sample st_coordinates
 #' @importFrom stargazer stargazer
 #' @importFrom ggplot2 ggplot geom_point aes ggtitle scale_color_viridis_d guides guide_legend theme_minimal theme element_text xlab ylab
 #'
-# ew uzupełnić inne importy!!!; może trzeba się pozbyć pakietu sp?
+# ew. uzupełnić inne importy!!!; może trzeba się pozbyć pakietu sp?
+# SPRAWDZIĆ @importFrom vs Imports w DESCRIPTION i funkcjami użytymi
 #'
 NULL
--- R/benford.R
+++ R/benford.R
@@ -23,6 +23,7 @@
 #' @export
 SpatBenfordTest<-function(data_sf, sample_size, var_name=NULL){
   # koordynaty w zależności od typu danych
+  #DODAĆ SPRAWDZENIE CZY SF W FORMIE POINTS!!!!
   if(inherits(data_sf,"sf")) crds<-sf::st_coordinates(data_sf)
   else if(inherits(data_sf,"data.frame",TRUE)==1){
     crds<-data_sf[,c(1,2)]
@@ -59,9 +60,9 @@ SpatBenfordTest<-function(data_sf, sample_size, var_name=NULL){
   data.d<-crds[sample(nrow(crds), sample_size, replace=FALSE), ]
 
   my.dist<-dist(data.d)
-  my.benford<-benford(as.vector(my.dist))
-  plot(my.benford)
-  return (my.benford)
+  benford_result<-benford(as.vector(my.dist))
+  plot(benford_result)
+  return (benford_result)
 }
 
 
@@ -74,5 +75,54 @@ SpatBenfordTest<-function(data_sf, sample_size, var_name=NULL){
 # generating spatial Benford-like 2D distribution
 ################################
 #
-# DODAĆ!!!!!
+# SPRAWDZIĆ CZY NIE OPRZEĆ NA PAKIECIE benford (nowszy) zamiast benford.analysis
+# DODAĆ TESTOWANIE WYNIKU ZA POMOCĄ SPATBENFORDPATTERN()
 #
+#' Linijka nr 1 - function title
+#'
+#' Linijka nr 2 - description
+#'
+#' Linijka nr 3 - details
+#'
+#'
+#' @name SpatBenfordPattern
+#' @aliases SpatBenfordPattern spatbenfordpattern
+#' @param region_sf do opisu (obiekt sf ale jako region)
+#' @param sample_size Number of points to be generated based on the region. Default value 5000. We suggest that a value greater than ..... is not used for reasons of computational efficiency. (SPRAWDZIĆ, UWAGA! st_sample czasem generuje mniej punktów)
+#' @references #To be done - ważne tutaj bo sztywne parametry w kodzie
+#' @examples #To be done!!!
+#'
+#' @return `SpatBenfordPattern()` returns ... to be done.
+#'
+#' @export
+SpatBenfordPattern<-function(region_sf, sample_size=5000){
+  #DODAĆ SPRAWDZENIE CZY SF W FORMIE REGIONU!!!!
+  if(!inherits(region_sf,"sf")) {
+    stop("The class of region_sf must be 'sf' only.")
+  }
+
+  ss_share<-c(0.810, 0.120, 0.070) # % of obs. clustered / regular /random
+  bbox<-st_bbox(region_sf)
+
+  # clustered skewed distribution
+  x<-rnorm(sample_size*ss_share[1], bbox[1]+0.25*(bbox[3]-bbox[1]), 0.08*(bbox[3]-bbox[1]))
+  y<-rnorm(sample_size*ss_share[1], bbox[2]+0.75*(bbox[4]-bbox[2]), 0.08*(bbox[4]-bbox[2]))
+  bpp1<-data.frame(X=x, Y=y)
+
+  bpp_sf2<-st_sample(region_sf, round(sample_size*ss_share[2],0), type="regular")
+  bpp2<-st_coordinates(bpp_sf2)
+
+  bpp_sf3<-st_sample(region_sf, round(sample_size*ss_share[3],0), type="random")
+  bpp3<-st_coordinates(bpp_sf3)
+
+  bpp<-rbind(bpp1, bpp2, bpp3)
+  bpp$class<-NA
+  bpp$class[1:nrow(bpp1)]<-"clustered"
+  bpp$class[(nrow(bpp1)+1):(nrow(bpp1)+nrow(bpp2))]<-"regular"
+  bpp$class[(nrow(bpp1)+nrow(bpp2)+1):nrow(bpp)]<-"random"
+
+  bpp_sf<-st_as_sf(bpp, coords=c("X", "Y"), crs=st_crs(region_sf), agr="constant")
+
+  plot(bpp_sf, key.pos=1, main="Benford Pattern Spatial Points")
+  return(bpp) # poprawić, żeby może na ekranie było jakieś summary/table a nie lista
+}
