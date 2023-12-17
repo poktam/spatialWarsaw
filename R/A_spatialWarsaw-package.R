029688921a305beba08d08588d810ef251d197a5
Mateusz Kopyt <mkopyt@wne.uw.edu.pl>
2023-12-17 23:50:51 +0100

    Plot in bootdbscan() added

--- R/A_spatialWarsaw-package.R
+++ R/A_spatialWarsaw-package.R
@@ -19,7 +19,7 @@
 #' @importFrom sf st_as_sf st_bbox st_crs st_sample st_coordinates st_geometry_type st_voronoi st_cast st_union st_intersection st_area st_geometry st_transform
 #' @importFrom stargazer stargazer
 #' @importFrom ggplot2 ggplot geom_point aes aes_string ggtitle scale_color_viridis_d guides guide_legend theme_minimal theme element_text xlab ylab
-#' @importFrom graphics legend par
+#' @importFrom graphics legend par points
 #'
 # ew. uzupełnić inne importy!!!; może trzeba się pozbyć pakietu sp?
 # SPRAWDZIĆ @importFrom vs Imports w DESCRIPTION i funkcjami użytymi
--- R/bootdbscan.R
+++ R/bootdbscan.R
@@ -11,7 +11,7 @@
 #'
 #' @name bootdbscan
 #' @aliases bootdbscan BOOTDBSCAN
-#' @param crds do opisu
+#' @param data_sf do opisu (obiekt sf lub data.frame - w data frame 1 kolumna musi być X coords, druga kolumna Y coords)
 #' @param sample_size do opisu
 #' @param times do opisu
 #' @param eps do opisu
@@ -27,27 +27,50 @@
 #' \item{minPts }{ value of the `minPts` parameter.}
 #'
 #' @export
-bootdbscan<-function(crds, sample_size, times, eps, minPts){
+bootdbscan<-function(data_sf, sample_size, times, eps, minPts){
+  if((inherits(data_sf,"sf") && st_geometry_type(data_sf,FALSE)=="POINT")) crds<-sf::st_coordinates(data_sf)
+  else if(inherits(data_sf,"data.frame",TRUE)==1){
+    crds<-data_sf[,c(1,2)]
+    colnames(crds)<-c("X","Y")
+  }
+  else {
+    stop("The class of data_sf must only be 'sf' of geometry type 'POINTS' or 'data.frame'.")
+  }
+
+  # zbadać sample_size i ustawić ew. na wielkość zbioru danych
+  sample_size <- as.integer(sample_size)
+  if (sample_size > nrow(crds) || sample_size<1) {
+    sample_size<-nrow(crds)
+    cat("Wrong sample size. Sample_size set to:",sample_size,"\n",sep="")
+  }
+
+  # zbadać times, czy nie ujemne - może inny warunek niż <=0
+  times<-round(times,0)
+  if (times<=0) {
+    times<-16
+    cat("Wrong number of clusters. Number of cluster set to the proposed value: 16.","\n",sep="")
+  }
 
   selector<-matrix(0, nrow=sample_size, ncol=times)
   result<-matrix(0, nrow=sample_size, ncol=times)
 
-  for(i in 1:times) selector[,i]<-sample(1:dim(crds)[1], sample_size, replace=FALSE) # sample points
+  for(i in 1:times) selector[,i]<-sample(1:nrow(crds), sample_size, replace=FALSE) # sample points
 
+  # poniższe można uprościć (trochę) - zmniejszyć liczbę pośrednich obiektów
   for(i in 1:times){
     vec<-selector[,i]
     sub<-as.matrix(crds[vec,])
-    dbs<-dbscan::dbscan(sub, eps=eps, minPts=minPts) # dbscan in subgroups
+    dbs<-dbscan(sub, eps=eps, minPts=minPts) # dbscan in subgroups
     result[,i]<-dbs$cluster}
 
-  DBS<-data.frame(ID=1:dim(crds)[1], new_cluster=rep(NA, times=dim(crds)[1]))
+  DBS<-data.frame(ID=1:nrow(crds), new_cluster=rep(NA, times=nrow(crds)))
 
   selector_v<-as.vector(selector)
   result_v<-as.vector(result)
   result_v<-ifelse(result_v>0, 1, 0)
   x<-cbind(selector_v, result_v)
 
-  x.grouped <- dplyr::summarise(dplyr::group_by(as.data.frame(x), selector_v), mean = mean(result_v))
+  x.grouped <- summarise(group_by(as.data.frame(x), selector_v), mean = mean(result_v))
 
   DBS$new_cluster[x.grouped$selector_v]<-round(x.grouped$mean,0) # for the points that were sampled at least once,
   # assign 0 if at average they were outliers, and 1
@@ -57,10 +80,14 @@ bootdbscan<-function(crds, sample_size, times, eps, minPts){
   nneigh = 1
 
   while(length(no_cluster)>0){  # as long as there are points with no assignment...
-    crds.knn<-dbscan::kNN(crds, nneigh, sort=FALSE) # ...find their next nearest neighbour...
+    crds.knn<-kNN(crds, nneigh, sort=FALSE) # ...find their next nearest neighbour...
     DBS$new_cluster[no_cluster]<-DBS$new_cluster[crds.knn$id[no_cluster,nneigh]] #...and assign the assignment of the neighbour
     nneigh = nneigh+1 # check by the further neighbour
     no_cluster<-which(is.na(DBS$new_cluster))}
+
+  # wykres do sprawdzenia czy o to chodziło, tytuł do sprawdzenia
+  plot(crds,pch=".", main="Observations with core points (red)")
+  points(crds[DBS$new_cluster==1,],pch=".",col="red")
 
   structure(
     list(
