d1b29610c9ceb6b133b1e99fb933b2ebb2a0e289
Mateusz Kopyt <mkopyt@wne.uw.edu.pl>
2024-03-30 00:34:40 +0100

    Minor correction in documentation for firms_sf dataset.

--- R/data.R
+++ R/data.R
@@ -9,7 +9,6 @@
 #' of Lubelskie.
 #'
 #' @format An object of class `sf` with projection EPSG:4326 (WGS84) consisting of the following variables:
-#' \describe{
 #' \item{empl}{Employment.}
 #' \item{roa}{Return on assets.}
 #' \item{dummy.agri}{Dummy variable indicating whether the enterprise belongs to the agricultural sector.}
@@ -17,11 +16,10 @@
 #' \item{dummy.constr}{Dummy variable indicating whether the enterprise belongs to the construction sector.}
 #' \item{dummy.serv}{Dummy variable indicating whether the enterprise belongs to the services sector.}
 #' \item{dist.big.city}{Distance (in km) to the nearest large city.}
-#' \item{dist.closest.city}{Distance [in km] to the nearest city.}
+#' \item{dist.closest.city}{Distance (in km) to the nearest city.}
 #' \item{year}{Year.}
 #' \item{sector.aggr}{Sector to which the enterprise belongs.}
 #' \item{sector}{Sector according to Polish Classification of Economic Activities (PKD 2007).}
-#' }
 #'
 #' @source Own work based on REGON registry (Statistics Poland),
 #' <https://wyszukiwarkaregon.stat.gov.pl/>
